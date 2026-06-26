b0a382f641e8981337b5a48c43b9e0006046af3d
AshishJ116 <jayswalashish965@gmail.com>
2026-06-26 16:39:59 +0545

    addtobags

--- src/customer/pages/Home/Grid/Grid.tsx
+++ src/customer/pages/Home/Grid/Grid.tsx
@@ -5,27 +5,27 @@ const Grid = () => {
     <div className='grid gap-4 grid-rows-12 grid-cols-12 lg:h-[600px] px-5 lg:px-20'>
       
         <div className='col-span-3 row-span-12 text-white rounded-md'>
-         <img className='w-full h-full object-cover rounded-md' src="https://www.arunvastrabhandar.com/images/cat/sherwani/6.jpg"/>
+         <img className='w-full h-full object-cover rounded-md' src="https://rukminim2.flixcart.com/image/1536/1536/xif0q/suit/m/e/b/36-new-black-suit-denigma-original-imahhqukzmh6uvhg.jpeg?q=90"/>
         </div>
 
         <div className='col-span-2 row-span-6 text-white rounded-md'>
-         <img className='w-full h-full object-cover rounded-md' src="https://www.arunvastrabhandar.com/images/cat/sherwani/6.jpg"/>
+         <img className='w-full h-full object-cover rounded-md' src="https://rukminim2.flixcart.com/image/1536/1536/xif0q/sari/m/t/s/free-bird-maroon-fa16-maroon-red-organza-tissue-silk-border-original-imahhjbzvcgwcyzb.jpeg?q=90"/>
         </div>
 
         <div className='col-span-4 row-span-6 text-white rounded-md'>
-         <img className='w-full h-full object-cover rounded-md' src="https://www.arunvastrabhandar.com/images/cat/sherwani/6.jpg"/>
+         <img className='w-full h-full object-cover rounded-md' src="https://rukminim2.flixcart.com/image/1536/1536/xif0q/remote-control-toy/s/g/k/rc-4x4-formula-f1-drift-smoke-car-toy-1-14-scale-2-4ghz-hand-original-imahmp3rzpkttjp3.jpeg?q=90"/>
         </div>
       
         <div className='col-span-3 row-span-12 text-white rounded-md'>
-         <img className='w-full h-full object-cover rounded-md' src="https://www.arunvastrabhandar.com/images/cat/sherwani/6.jpg"/>
+         <img className='w-full h-full object-cover rounded-md' src="https://rukminim2.flixcart.com/image/1536/1536/xif0q/sari/z/0/i/free-saree-new-design-saree-new-design-2025-party-wear-saree-original-imahdh7afmgu29rp.jpeg?q=90"/>
         </div>
       
           <div className='col-span-4 row-span-6 text-white rounded-md'>
-         <img className='w-full h-full object-cover rounded-md' src="https://www.arunvastrabhandar.com/images/cat/sherwani/6.jpg"/>
+         <img className='w-full h-full object-cover rounded-md' src="https://rukminim2.flixcart.com/image/1536/1536/xif0q/television/4/o/6/marq-by-flipkart-32hdndqee1b-marq-by-flipkart-original-imah6fc3hqjdtbds.jpeg?q=90"/>
         </div>
       
         <div className='col-span-2 row-span-6 text-white rounded-md'>
-         <img className='w-full h-full object-cover rounded-md' src="https://www.arunvastrabhandar.com/images/cat/sherwani/6.jpg"/>
+         <img className='w-full h-full object-cover rounded-md' src="https://rukminim2.flixcart.com/image/1536/1536/xif0q/shoe/e/i/q/6-sport-24503-6-nobelite-white-black-original-imahz5hw8tz6heyp.jpeg?q=90"/>
         </div>
     </div>
   )
